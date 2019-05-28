e6e8de14d0b28743cb26ac8f0847c7a6898d8321
Harold Alcala <harold.alcala@vcube.co.jp>
2019-05-28 10:28:29 +0800

    Logging

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import { startServer } from "./server";
 import { my_util } from "./MyUtil";
 import { DefaultAuthHandler } from "./modules/auth/DefaultAuthHandler";
 
-const { logDebug, logWarn, logInfo } = my_util.getLoggers(module, 4);
+const { logDebug, logWarn, logInfo, logError } = my_util.getLoggers(module, 4);
 
 const auth_provider = new DefaultAuthHandler(); // replace this with your custom auth provider
 
 startServer(auth_provider).catch(err => {
-	logDebug.enabled && logDebug("index.ts:: err:", err);
+  logError("index.ts:: err:", err);
 });
